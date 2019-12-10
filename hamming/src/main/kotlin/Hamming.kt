1ef0256115979d6c93b3f748ef4adc1d38de7506
sup95 <sup0508@gmail.com>
2019-12-10 19:04:41 +0530

    complete Hamming

---
+++ hamming/src/main/kotlin/Hamming.kt
@@ -0,0 +1,19 @@
+object Hamming {
+
+    fun compute(leftStrand: String, rightStrand: String): Int {
+        /*if(leftStrand.length != rightStrand.length)
+            throw IllegalArgumentException("left and right strands must be of equal length")
+
+        var count = 0
+        for(i in leftStrand.indices) {
+            if(leftStrand[i] != rightStrand[i])
+                count++
+        }
+
+        return count*/
+
+        require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
+
+        return leftStrand.zip(rightStrand).count{ it.first != it.second }
+    }
+}
