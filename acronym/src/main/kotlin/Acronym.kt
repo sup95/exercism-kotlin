f5006e349d2ed3190389b8754369024b91987650
sup95 <sup0508@gmail.com>
2020-07-05 20:54:51 +0530

    improved acronym solution

--- acronym/src/main/kotlin/Acronym.kt
+++ acronym/src/main/kotlin/Acronym.kt
@@ -1,7 +1,20 @@
 object Acronym {
-    fun generate(phrase: String) : String {
+    /*fun generate(phrase: String) : String {
         val acronymInTheMaking = phrase.split(" ").map { it.split("-").map { it.dropWhile { !it.isLetter() }.firstOrNull() ?: "" } }
         val acronymResult = acronymInTheMaking.flatMap { it.toList() }.joinToString("").toUpperCase()
         return acronymResult
+    }*/
+
+    fun generate(phrase: String) : String {
+        val acronymInTheMaking =
+                phrase.replace("-", " ")
+                        .split(" ")
+                        .map {
+                            it.dropWhile { !it.isLetter() }
+                                    .firstOrNull() ?: ""
+                        }
+        val acronymResult = acronymInTheMaking.joinToString("").toUpperCase()
+        return acronymResult
     }
+
 }
